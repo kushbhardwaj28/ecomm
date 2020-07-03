7c978106a7bd7ef610ae52b1af5a10ff03ace73e
Kush Bhardwaj <bhardwaj.kush69@gmail.com>
2020-07-03 16:29:03 +0530

    remove clientId

--- server/passport_oauth/passport_setup.js
+++ server/passport_oauth/passport_setup.js
@@ -14,54 +14,26 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((user, done) => {
-    // User.findById(id, function (err, user) {
     done(null, user);
-    // });
 });
 
 passport.use(new GoogleStrategy({
-    clientID: "113817837187-q43nrc6fc87e28c9ep4ms50nmigfj5rp.apps.googleusercontent.com",
-    clientSecret: "PH7GLYiMfwL31uj7tLsXPTDc",
+    clientID: "google-client-id",
+    clientSecret: "google-secret",
     callbackURL: "http://localhost:3000/login/google/callback"
 },
     (accessToken, refreshToken, profile, done) => {
-        // User.findOrCreate({ googleId: profile.id }, function (err, user) {
         return sellerFindOrCreate(profile, connection, done);
-        // return done(null, profile);
-        // });
     }
 ));
 
 passport.use(new FacebookStrategy({
-    clientID: "638051787057565",
-    clientSecret: "26547af60b264af774f239781fa22027",
+    clientID: "fb-client-id",
+    clientSecret: "fb-client-secret",
     callbackURL: "http://localhost:3000/login/facebook/callback",
     profileFields: ['id', 'email', 'link', 'name'],
 },
     function (accessToken, refreshToken, profile, done) {
         return sellerFindOrCreate(profile, connection, done);
-        // getUser(profile.emails[0].value, connection, (result, err) => {
-        //     if (err) {
-        //         console.log(err);
-        //         throw err;
-        //     }
-        //     if (result.length > 0) {
-        //         return done(null, result[0]);
-        //     } else {
-        //         return createUser(
-        //             profile.name.givenName + ' ' + profile.name.familyName,
-        //             profile.emails[0].value, connection,
-        //             // profile.
-        //             (result, err) => {
-        //                 if (err) {
-        //                     console.log(err);
-        //                     throw err;
-        //                 }
-        //                 done(null, result[0]);
-        //             });
-        //     }
-        // });
-        // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-        // });
     }
 ));
